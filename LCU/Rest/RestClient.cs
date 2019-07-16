d039b55f4c2535fef26f81fc4e4fdbc573a7849b
mcgear <michael.gearhardt@fathym.com>
2019-07-15 23:51:45 -0600

    Base RestClient seed

---
+++ LCU/Rest/RestClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCU.Rest
+{
+	public class RestClient
+	{
+		#region Fields
+		protected readonly HttpClient web;
+		#endregion
+
+		#region Constructors
+		public RestClient(string apiRoot)
+		{
+			web = new HttpClient();
+
+			web.BaseAddress = new Uri(apiRoot);
+		}
+		#endregion
+
+		#region API Methods
+		public virtual async Task<T> Get<T>(string requestUri)
+			where T : class
+		{
+			var entCtxtStr = await web.GetStringAsync(requestUri);
+
+			return entCtxtStr?.FromJSON<T>();
+		}
+		#endregion
+	}
+}
