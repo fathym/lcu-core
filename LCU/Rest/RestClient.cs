37e7839c5c95bd0f2d0230218ffc12b4e1aeae79
mcgear <michael.gearhardt@fathym.com>
2019-07-16 00:21:22 -0600

    More rest client roots

--- LCU/Rest/RestClient.cs
+++ LCU/Rest/RestClient.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace LCU.Rest
@@ -22,12 +20,48 @@ namespace LCU.Rest
 		#endregion
 
 		#region API Methods
+		public virtual async Task<T> Delete<T>(string requestUri)
+			where T : class
+		{
+			var response = await web.DeleteAsync(requestUri);
+
+			var respStr = await response.Content.ReadAsStringAsync();
+
+			return respStr?.FromJSON<T>();
+		}
+
 		public virtual async Task<T> Get<T>(string requestUri)
 			where T : class
 		{
-			var entCtxtStr = await web.GetStringAsync(requestUri);
+			var respStr = await web.GetStringAsync(requestUri);
 
-			return entCtxtStr?.FromJSON<T>();
+			return respStr?.FromJSON<T>();
+		}
+
+		public virtual async Task<TResp> Post<TReq, TResp>(string requestUri, TReq request)
+			where TResp : class
+		{
+			var response = await web.PostAsJsonAsync(requestUri, request);
+
+			var respStr = await response.Content.ReadAsStringAsync();
+
+			return respStr?.FromJSON<TResp>();
+		}
+
+		public virtual async Task<TResp> Put<TReq, TResp>(string requestUri, TReq request)
+			where TResp : class
+		{
+			var response = await web.PutAsJsonAsync(requestUri, request);
+
+			var respStr = await response.Content.ReadAsStringAsync();
+
+			return respStr?.FromJSON<TResp>();
+		}
+
+		public virtual async Task<TResp> With<TResp>(Func<HttpClient, Task<TResp>> action)
+			where TResp : class
+		{
+			return await action(web);
 		}
 		#endregion
 	}
