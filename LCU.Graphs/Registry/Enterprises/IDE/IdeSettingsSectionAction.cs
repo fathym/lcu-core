32b30cd30f4a180cb0a9fd733da6e9d4e4b053d9
mcgear <michael.gearhardt@fathym.com>
2019-08-10 23:07:56 -0600

    Cleaning up data models

---
+++ LCU.Graphs/Registry/Enterprises/IDE/IdeSettingsSectionAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace LCU.Graphs.Registry.Enterprises.IDE
+{
+	[Serializable]
+	[DataContract]
+	public class IdeSettingsSectionAction
+	{
+		[DataMember]
+		public virtual string Action { get; set; }
+
+		[DataMember]
+		public virtual string Group { get; set; }
+
+		[DataMember]
+		public virtual string Name { get; set; }
+	}
+}
