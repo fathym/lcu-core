ec1da7016c8361afd982fcc9c485d1b5c3fc883b
mcgear <michael.gearhardt@fathym.com>
2019-09-17 17:49:14 -0600

    Data Flow model

---
+++ LCU.Graphs/Registry/Enterprises/Provisioning/DataFlow.cs
@@ -0,0 +1,24 @@
+﻿using Fathym.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace LCU.Graphs.Registry.Enterprises.Provisioning
+{
+	[Serializable]
+	[DataContract]
+	public class DataFlow : BusinessModel<Guid>
+	{
+		[DataMember]
+		public virtual string Description { get; set; }
+
+		[DataMember]
+		public virtual string EnterpriseAPIKey { get; set; }
+
+		[DataMember]
+		public virtual string Lookup { get; set; }
+
+		[DataMember]
+		public virtual string Name { get; set; }
+	}
+}
