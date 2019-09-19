195152f9bbc661b3df972e687f0a5a21e2c074c3
mcgear <michael.gearhardt@fathym.com>
2019-09-19 00:03:03 -0600

    Interface update

--- LCU.Graphs/Registry/Enterprises/DataFlows/IDataFlowGraph.cs
+++ LCU.Graphs/Registry/Enterprises/DataFlows/IDataFlowGraph.cs
@@ -7,7 +7,11 @@ namespace LCU.Graphs.Registry.Enterprises.DataFlows
 {
 	public interface IDataFlowGraph
 	{
+		Task<Status> DeleteDataFlow(string apiKey, string envLookup, string dfLookup);
+
 		Task<DataFlow> GetDataFlow(string apiKey, string envLookup, string dfLookup);
+
+		Task<List<DataFlow>> ListDataFlows(string apiKey, string envLookup);
 
 		Task<DataFlow> SaveDataFlow(string apiKey, string envLookup, DataFlow dataFlow);
 	}
