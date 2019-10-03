2e9269767f694b469284717c5df34e3031bc55fc
mcgear <michael.gearhardt@fathym.com>
2019-10-03 10:42:38 -0600

    Enable infinite timout by default and added support for request based Timeout setting

--- LCU/Rest/LCURestClient.cs
+++ LCU/Rest/LCURestClient.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LCU.Rest
@@ -19,7 +20,14 @@ namespace LCU.Rest
 		{
 			this.logger = logger;
 
-			web = new HttpClient(new LoggingHandler(new HttpClientHandler(), this.logger));
+			var handler = new TimeoutHandler
+			{
+				InnerHandler = new LoggingHandler(new HttpClientHandler(), this.logger)
+			};
+
+			web = new HttpClient(handler);
+
+			web.Timeout = Timeout.InfiniteTimeSpan;
 
 			if (!bearerToken.IsNullOrEmpty())
 				web.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", bearerToken);
@@ -76,6 +84,57 @@ namespace LCU.Rest
 			where TResp : class
 		{
 			return await action(web);
+		}
+		#endregion
+	}
+
+	public class TimeoutHandler : DelegatingHandler
+	{
+		#region Properties
+		public virtual TimeSpan DefaultTimeout { get; set; }
+		#endregion
+
+		#region Constructors
+		public TimeoutHandler()
+		{
+			DefaultTimeout = TimeSpan.FromSeconds(100);
+		}
+		#endregion
+
+		#region Helpers
+		protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			try
+			{
+				using (var cts = getCancellationTokenSource(request, cancellationToken))
+				{
+					return await base.SendAsync(request, cts?.Token ?? cancellationToken);
+				}
+			}
+			catch (OperationCanceledException)
+				when (!cancellationToken.IsCancellationRequested)
+			{
+				throw new TimeoutException();
+			}
+		}
+
+		protected virtual CancellationTokenSource getCancellationTokenSource(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			var timeout = request.GetTimeout() ?? DefaultTimeout;
+
+			if (timeout == Timeout.InfiniteTimeSpan)
+			{
+				// No need to create a CTS if there's no timeout
+				return null;
+			}
+			else
+			{
+				var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+
+				cts.CancelAfter(timeout);
+
+				return cts;
+			}
 		}
 		#endregion
 	}
