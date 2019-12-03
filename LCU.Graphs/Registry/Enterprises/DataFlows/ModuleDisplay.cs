b4cefc718bbef2c6cc6f885c1f12ceed9955f7af
mcgear <michael.gearhardt@gmail.com>
2019-12-03 16:40:59 -0700

    Bug fix for wrong type

--- LCU.Graphs/Registry/Enterprises/DataFlows/ModuleDisplay.cs
+++ LCU.Graphs/Registry/Enterprises/DataFlows/ModuleDisplay.cs
@@ -23,7 +23,7 @@ namespace LCU.Graphs.Registry.Enterprises.DataFlows
 		public virtual double Height { get; set; }
 
 		[DataMember]
-		public virtual ApplicationProfile Icon { get; set; }
+		public virtual LCUIcon Icon { get; set; }
 
 		[DataMember]
 		public virtual double Left { get; set; }
