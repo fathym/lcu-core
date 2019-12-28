a695b1ef7c7bd184f5545cabbc6748654512cd48
kevin.bowers <kevin.bowers@fathym.com>
2019-12-27 21:35:39 -0700

    flow type dependency

---
+++ LCU.Graphs/Registry/Enterprises/DataFlows/FlowTypeDependency.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace LCU.Graphs.Registry.Enterprises.DataFlows
+{
+    [DataContract]
+    public class FlowTypeDependency
+    {
+
+        [DataMember]
+        public virtual string ModuleID { get; set; }
+
+        [DataMember]
+        public virtual string ModuleName { get; set; }
+
+        [DataMember]
+        public virtual string MasterID { get; set; }
+
+        [DataMember]
+        public virtual string SchemaID { get; set; }
+
+        [DataMember]
+        public virtual string SchemaName { get; set; }
+
+        [DataMember]
+        public virtual string Type { get; set; }
+    }
+}
+
