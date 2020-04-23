8561c0eb78bb5d3998058675229d1eca9fb3de9e
Matt Brooks <matt.brooks@fathym.com>
2020-04-23 16:21:36 -0600

    changing to diff signupsignin policy

--- LCU.Presentation/Identity/LCUOAUTHConstants.cs
+++ LCU.Presentation/Identity/LCUOAUTHConstants.cs
@@ -8,7 +8,7 @@ namespace LCU.Presentation.Identity
     {
         public const string SignUpPolicy = "B2C_1_LCU_SignUp";
         public const string SignInPolicy = "B2C_1_LCU_SignIn";
-        public const string SignUpSignInPolicy = "B2C_1_LCU_SignUp_SignIn";
+        public const string SignUpSignInPolicy = "B2C_1A_SignUp_SignIn";
         public const string EditProfilePolicy = "B2C_1_LCU_Edit_Profile";
         public const string OAUTHPolicySessionKey = "OAUTH:Process";
         public const string OAUTHTokenSessionKey = "OAUTH:Token";
