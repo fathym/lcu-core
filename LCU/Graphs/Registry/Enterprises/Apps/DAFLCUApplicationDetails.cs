1d7f977e0e6be7e77e73497fd8914222d0483370
mcgear <michael.gearhardt@gmail.com>
2020-10-01 09:29:17 -0600

    New model

---
+++ LCU/Graphs/Registry/Enterprises/Apps/DAFLCUApplicationDetails.cs
@@ -0,0 +1,25 @@
+﻿using Fathym;
+using Fathym.Business.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace LCU.Graphs.Registry.Enterprises.Apps
+{
+    [Serializable]
+    [DataContract]
+    public class DAFLCUApplicationDetails : MetadataModel
+    {
+        [DataMember]
+        public virtual string Lookup { get; set; }
+
+        [DataMember]
+        public virtual string NPMPackage { get; set; }
+
+        [DataMember]
+        public virtual string PackageVersion { get; set; }
+    }
+}
