119146be26989eff0dd6fd3c70c3262e2bcdb034
mcgear <michael.gearhardt@gmail.com>
2021-03-24 23:10:26 -0600

    New props and classes

--- LCU/Graphs/Registry/Enterprises/Apps/DAFRedirectApplicationDetails.cs
+++ LCU/Graphs/Registry/Enterprises/Apps/DAFRedirectApplicationDetails.cs
@@ -10,6 +10,12 @@ namespace LCU.Graphs.Registry.Enterprises.Apps
 	public class DAFRedirectApplicationDetails
 	{
 		[DataMember]
+		public virtual bool Permanent { get; set; }
+
+		[DataMember]
+		public virtual bool PreserveMethod { get; set; }
+
+		[DataMember]
 		public virtual string Redirect { get; set; }
 	}
 }
