1bf15dfc67bbc3bed6356e8f403f5fc4ae944f17
Michael Gearhardt <michael.gearhardt@fathym.com>
2022-05-19 11:07:18 -0600

    Shifted around some code to ensure some hosting stuff is still available in netstandard2.1 packages

--- LCU.Hosting.OpenId/LCUStartupExtensions.cs
+++ LCU.Hosting.OpenId/LCUStartupExtensions.cs
@@ -17,209 +17,6 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.Json;
 
-namespace Microsoft.Extensions.DependencyInjection
-{
-    public static class LCUStartupServiceExtensions
-    {
-        #region API Methods
-        public static IServiceCollection AddLCUAPIPipeline(this IServiceCollection services,
-            LCUStartupGlobalPipelineOptions globalOpts, IConfiguration config)
-        {
-            if (globalOpts != null)
-            {
-                services.AddLCUDebug(globalOpts.Debug);
-
-                services.AddLCUExceptions(globalOpts.Exceptions);
-
-                services.AddLCUCaching(globalOpts.Caching, config);
-
-                services.AddLCUForwardedHeaders(globalOpts.UseForwardedHeaders);
-
-                services.AddLCUAPI(globalOpts.API);
-
-                services.AddLCUURLRewriter(globalOpts.URLRewriter);
-
-                services.AddLogging();
-
-                services.AddHttpContextAccessor();
-
-                services.AddApplicationInsightsTelemetry(globalOpts.ApplicationInsights);
-            }
-
-            return services;
-        }
-
-        #region Add Helpers
-        public static void AddConfig<T>(this IServiceCollection services, string key, IConfiguration config)
-            where T : class, new()
-        {
-            var configSec = config.GetSection(key);
-
-            services.Configure<T>(configSec);
-
-            var cfg = new T();
-
-            configSec.Bind(cfg);
-
-            services.AddSingleton(cfg);
-        }
-
-        public static void AddLCUAPI(this IServiceCollection services, LCUStartupAPIOptions apiOpts)
-        {
-            if (apiOpts != null)
-            {
-                services.AddControllers()
-                    .AddJsonOptions(o =>
-                    {
-                        DesignOutline.Instance.BuildCommonDefaultJSONSerialization(o.JsonSerializerOptions);
-
-                        if (apiOpts.EnableCamelCasing)
-                            o.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-                    });
-                    //.AddNewtonsoftJson(o =>
-                    //{
-                    //    if (!apiOpts.EnableCamelCasing)
-                    //        o.SerializerSettings.ContractResolver = new DefaultContractResolver();
-                    //});
-
-                if (apiOpts.Swagger != null)
-                {
-                    services.AddSwaggerGen(c =>
-                    {
-                        c.SwaggerDoc(apiOpts.Swagger.Info.Version, apiOpts.Swagger.Info);
-                    });
-
-                    //services.AddSwaggerGenNewtonsoftSupport();
-                }
-            }
-        }
-
-        public static void AddLCUCaching(this IServiceCollection services, LCUStartupCachingOptions cacheOpts, IConfiguration config)
-        {
-            if (cacheOpts != null)
-            {
-                services.AddMemoryCache();
-
-                if (cacheOpts.Redis == null)
-                    services.AddDistributedMemoryCache();
-                else
-                    services.AddStackExchangeRedisCache(options =>
-                    {
-                        options.Configuration = config[cacheOpts.Redis.Configuration] ?? cacheOpts.Redis.Configuration;
-
-                        options.InstanceName = cacheOpts.Redis.InstanceName;
-                    });
-            }
-        }
-
-        public static void AddLCUDebug(this IServiceCollection services, LCUStartupDebugOptions debugOpts)
-        {
-        }
-
-        public static void AddLCUExceptions(this IServiceCollection services, LCUStartupExceptionOptions exOpts)
-        {
-        }
-
-        public static void AddLCUForwardedHeaders(this IServiceCollection services, bool useForwardedHeaders)
-        {
-            services.Configure<ForwardedHeadersOptions>(options =>
-            {
-                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedHost | ForwardedHeaders.XForwardedProto;
-
-                //  Todo: restrict to something ... 147.243.119.241
-                options.KnownNetworks.Clear();
-                options.KnownProxies.Clear();
-            });
-        }
-
-        public static IHttpClientBuilder AddLCUHTTPClient<TClient>(this IServiceCollection services,
-            IPolicyRegistry<string> registry, LCUStartupHTTPClientOptions httpOpts)
-            where TClient : class
-        {
-            var clientName = typeof(TClient).Name;
-
-            var clientOptions = httpOpts.Options[clientName];
-
-            return clientOptions.BaseAddress.IsNullOrEmpty() ? null : services.AddLCUHTTPClient<TClient>(registry, new Uri(clientOptions.BaseAddress));
-        }
-
-        public static IHttpClientBuilder AddLCUHTTPClient<TClient>(this IServiceCollection services,
-            IPolicyRegistry<string> registry, Uri baseAddress, int retryCycles = 3, int retrySleepDurationMilliseconds = 500,
-            int circuitFailuresAllowed = 5, int circuitBreakDurationSeconds = 5)
-            where TClient : class
-        {
-            return services
-                .AddRefitClient<TClient>(services =>
-                {
-                    return new RefitSettings()
-                    {
-                        //ContentSerializer = new NewtonsoftJsonContentSerializer()
-                    };
-                })
-                .ConfigureHttpClient(client =>
-                {
-                    client.BaseAddress = baseAddress;
-                });
-                //.AddLCUTimeoutPolicy(registry)
-                //.AddTransientHttpErrorPolicy(p =>
-                //{
-                //    return p.WaitAndRetryAsync(retryCycles, _ =>
-                //    {
-                //        return TimeSpan.FromMilliseconds(retrySleepDurationMilliseconds);
-                //    });
-                //})
-                //.AddTransientHttpErrorPolicy(p =>
-                //{
-                //    return p.CircuitBreakerAsync(circuitFailuresAllowed,
-                //        TimeSpan.FromSeconds(circuitBreakDurationSeconds));
-                //});
-        }
-
-        public static IPolicyRegistry<string> AddLCUPollyRegistry(this IServiceCollection services,
-            LCUStartupHTTPClientOptions httpOpts)
-        {
-            var registry = services.AddPolicyRegistry();
-
-            if (httpOpts != null)
-            {
-                var timeout = Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(httpOpts.TimeoutSeconds));
-
-                registry.Add("regular", timeout);
-
-                var longTimeout = Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(httpOpts.LongTimeoutSeconds));
-
-                registry.Add("long", longTimeout);
-            }
-
-            return registry;
-        }
-
-        public static IHttpClientBuilder AddLCUTimeoutPolicy(this IHttpClientBuilder httpClientBuilder,
-            IPolicyRegistry<string> registry)
-        {
-            return httpClientBuilder
-                .AddPolicyHandler(request =>
-                {
-                    var timeoutPolicy = "regular";
-
-                    if (request.Method != HttpMethod.Get)
-                        timeoutPolicy = "long";
-
-                    return registry.Get<IAsyncPolicy<HttpResponseMessage>>(timeoutPolicy);
-                });
-        }
-
-        public static void AddLCUURLRewriter(this IServiceCollection services, LCUStartupURLRewriterOptions urlRewriteOpts)
-        {
-            if (urlRewriteOpts != null)
-            {
-            }
-        }
-        #endregion
-        #endregion
-    }
-}
-
 namespace Microsoft.AspNetCore.Builder
 {
     public static class LCURuntimeStartupPipelineExtensions
@@ -341,7 +138,7 @@ namespace Microsoft.AspNetCore.Builder
             //{
             //    logger.LogInformation($"Using developer exception page");
 
-                app.UseDeveloperExceptionPage();
+            app.UseDeveloperExceptionPage();
             //}
             //else if (!exOpts.HandlerPath.IsNullOrEmpty())
             //{
@@ -389,7 +186,210 @@ namespace Microsoft.AspNetCore.Builder
                     options = options.AddRedirectToWwwPermanent();
 
                     app.UseRewriter(options);
-                }             
+                }
+            }
+        }
+        #endregion
+        #endregion
+    }
+}
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class LCUStartupServiceExtensions
+    {
+        #region API Methods
+        public static IServiceCollection AddLCUAPIPipeline(this IServiceCollection services,
+            LCUStartupGlobalPipelineOptions globalOpts, IConfiguration config)
+        {
+            if (globalOpts != null)
+            {
+                services.AddLCUDebug(globalOpts.Debug);
+
+                services.AddLCUExceptions(globalOpts.Exceptions);
+
+                services.AddLCUCaching(globalOpts.Caching, config);
+
+                services.AddLCUForwardedHeaders(globalOpts.UseForwardedHeaders);
+
+                services.AddLCUAPI(globalOpts.API);
+
+                services.AddLCUURLRewriter(globalOpts.URLRewriter);
+
+                services.AddLogging();
+
+                services.AddHttpContextAccessor();
+
+                services.AddApplicationInsightsTelemetry(globalOpts.ApplicationInsights);
+            }
+
+            return services;
+        }
+
+        #region Add Helpers
+        public static void AddConfig<T>(this IServiceCollection services, string key, IConfiguration config)
+            where T : class, new()
+        {
+            var configSec = config.GetSection(key);
+
+            services.Configure<T>(configSec);
+
+            var cfg = new T();
+
+            configSec.Bind(cfg);
+
+            services.AddSingleton(cfg);
+        }
+
+        public static void AddLCUAPI(this IServiceCollection services, LCUStartupAPIOptions apiOpts)
+        {
+            if (apiOpts != null)
+            {
+                services.AddControllers()
+                    .AddJsonOptions(o =>
+                    {
+                        DesignOutline.Instance.BuildCommonDefaultJSONSerialization(o.JsonSerializerOptions);
+
+                        if (apiOpts.EnableCamelCasing)
+                            o.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                    });
+                //.AddNewtonsoftJson(o =>
+                //{
+                //    if (!apiOpts.EnableCamelCasing)
+                //        o.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                //});
+
+                if (apiOpts.Swagger != null)
+                {
+                    services.AddSwaggerGen(c =>
+                    {
+                        c.SwaggerDoc(apiOpts.Swagger.Info.Version, apiOpts.Swagger.Info);
+                    });
+
+                    //services.AddSwaggerGenNewtonsoftSupport();
+                }
+            }
+        }
+
+        public static void AddLCUCaching(this IServiceCollection services, LCUStartupCachingOptions cacheOpts, IConfiguration config)
+        {
+            if (cacheOpts != null)
+            {
+                services.AddMemoryCache();
+
+                if (cacheOpts.Redis == null)
+                    services.AddDistributedMemoryCache();
+                else
+                    services.AddStackExchangeRedisCache(options =>
+                    {
+                        options.Configuration = config[cacheOpts.Redis.Configuration] ?? cacheOpts.Redis.Configuration;
+
+                        options.InstanceName = cacheOpts.Redis.InstanceName;
+                    });
+            }
+        }
+
+        public static void AddLCUDebug(this IServiceCollection services, LCUStartupDebugOptions debugOpts)
+        {
+        }
+
+        public static void AddLCUExceptions(this IServiceCollection services, LCUStartupExceptionOptions exOpts)
+        {
+        }
+
+        public static void AddLCUForwardedHeaders(this IServiceCollection services, bool useForwardedHeaders)
+        {
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedHost | ForwardedHeaders.XForwardedProto;
+
+                //  Todo: restrict to something ... 147.243.119.241
+                options.KnownNetworks.Clear();
+                options.KnownProxies.Clear();
+            });
+        }
+
+        public static IHttpClientBuilder AddLCUHTTPClient<TClient>(this IServiceCollection services,
+            IPolicyRegistry<string> registry, LCUStartupHTTPClientOptions httpOpts)
+            where TClient : class
+        {
+            var clientName = typeof(TClient).Name;
+
+            var clientOptions = httpOpts.Options[clientName];
+
+            return clientOptions.BaseAddress.IsNullOrEmpty() ? null : services.AddLCUHTTPClient<TClient>(registry, new Uri(clientOptions.BaseAddress));
+        }
+
+        public static IHttpClientBuilder AddLCUHTTPClient<TClient>(this IServiceCollection services,
+            IPolicyRegistry<string> registry, Uri baseAddress, int retryCycles = 3, int retrySleepDurationMilliseconds = 500,
+            int circuitFailuresAllowed = 5, int circuitBreakDurationSeconds = 5)
+            where TClient : class
+        {
+            return services
+                .AddRefitClient<TClient>(services =>
+                {
+                    return new RefitSettings()
+                    {
+                        //ContentSerializer = new NewtonsoftJsonContentSerializer()
+                    };
+                })
+                .ConfigureHttpClient(client =>
+                {
+                    client.BaseAddress = baseAddress;
+                });
+            //.AddLCUTimeoutPolicy(registry)
+            //.AddTransientHttpErrorPolicy(p =>
+            //{
+            //    return p.WaitAndRetryAsync(retryCycles, _ =>
+            //    {
+            //        return TimeSpan.FromMilliseconds(retrySleepDurationMilliseconds);
+            //    });
+            //})
+            //.AddTransientHttpErrorPolicy(p =>
+            //{
+            //    return p.CircuitBreakerAsync(circuitFailuresAllowed,
+            //        TimeSpan.FromSeconds(circuitBreakDurationSeconds));
+            //});
+        }
+
+        public static IPolicyRegistry<string> AddLCUPollyRegistry(this IServiceCollection services,
+            LCUStartupHTTPClientOptions httpOpts)
+        {
+            var registry = services.AddPolicyRegistry();
+
+            if (httpOpts != null)
+            {
+                var timeout = Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(httpOpts.TimeoutSeconds));
+
+                registry.Add("regular", timeout);
+
+                var longTimeout = Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(httpOpts.LongTimeoutSeconds));
+
+                registry.Add("long", longTimeout);
+            }
+
+            return registry;
+        }
+
+        public static IHttpClientBuilder AddLCUTimeoutPolicy(this IHttpClientBuilder httpClientBuilder,
+            IPolicyRegistry<string> registry)
+        {
+            return httpClientBuilder
+                .AddPolicyHandler(request =>
+                {
+                    var timeoutPolicy = "regular";
+
+                    if (request.Method != HttpMethod.Get)
+                        timeoutPolicy = "long";
+
+                    return registry.Get<IAsyncPolicy<HttpResponseMessage>>(timeoutPolicy);
+                });
+        }
+
+        public static void AddLCUURLRewriter(this IServiceCollection services, LCUStartupURLRewriterOptions urlRewriteOpts)
+        {
+            if (urlRewriteOpts != null)
+            {
             }
         }
         #endregion
