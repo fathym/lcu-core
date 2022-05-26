f455acbee044bd9103902b7c2a6dbe65d9cff345
Michael Gearhardt <michael.gearhardt@fathym.com>
2022-05-26 10:46:18 -0600

    Upgrades and remove runtime serialization

--- LCU/LCUIcon.cs
+++ LCU/LCUIcon.cs
@@ -1,19 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LCU
 {
-	[DataContract]
 	public class LCUIcon
 	{
-		[DataMember]
 		public virtual string Icon { get; set; }
 
-		[DataMember]
 		public virtual string IconSet { get; set; }
 	}
 }
